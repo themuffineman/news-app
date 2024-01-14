ef244bd22af2a9f99039e55d6bbd74a1b7adcfef
Petrus <petrusheya@gmail.com>
2024-01-14 13:14:22 +0200

    Profile and ProfileDropdown done

--- news/src/App.tsx
+++ news/src/App.tsx
@@ -1,5 +1,6 @@
 // import React from 'react'
 import NewsCard from './components/NewsCard'
+import Profile from './components/Profile'
 import SearchBar from './components/SearchBar'
 import SourcesCard from './components/SourcesCard'
 import TrendingArticle from './components/TrendingArticle'
@@ -7,6 +8,7 @@ const App = () => {
 
   return (
     <div className='flex flex-col items-center justify-between py-5 gap-2'>
+      <Profile/>
       <SearchBar/>
       <NewsCard/>
       <SourcesCard/>
