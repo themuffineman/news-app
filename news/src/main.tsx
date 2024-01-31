66001b33ecf1b92b8385b829b06fd02dcad9f80a
Petrus <petrusheya@gmail.com>
2024-01-31 21:47:47 +0200

    Main file context provision

--- news/src/main.tsx
+++ news/src/main.tsx
@@ -1,10 +1,6 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
-import Layout from './Layout.tsx';
-import SearchPage from './pages/SearchPage.tsx';
-import SignUpPage from './pages/SignUpPage.tsx';
 import App from './App.tsx';
 import { UserContext } from './utils/context.ts';
 
@@ -12,18 +8,13 @@ const Main = () => {
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
   const [userData, setUserData] = useState(null);
 
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path='/' element={ isLoggedIn ? ( <Layout />) : ( <UserContext.Provider value={{ userData, setUserData, setIsLoggedIn }}>  <SignUpPage /> </UserContext.Provider>)}>
-        <Route path='' element={<App />} />
-        <Route path='search/:searchQuery' element={<SearchPage />} />
-      </Route>
-    )
-  );
+
 
   return (
     <React.StrictMode>
-      <RouterProvider router={router} />
+      <UserContext.Provider value={{userData , setUserData, isLoggedIn, setIsLoggedIn}}>
+      <App />
+      </UserContext.Provider>
     </React.StrictMode>
   );
 };
